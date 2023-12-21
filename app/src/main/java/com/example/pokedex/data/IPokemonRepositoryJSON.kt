e03d963f18d0ba1f3ed76f19b0218527e957f936
BlueeLighty00 <davidsernamateu@gmail.com>
2023-12-21 19:57:34 +0100

    Dependency Injection

--- app/src/main/java/com/example/pokedex/data/IPokemonRepositoryJSON.kt
+++
@@ -1,8 +0,0 @@
-package com.example.pokedex.data
-
-import android.content.Context
-import com.example.pokedex.data.model.Pokemon
-
-interface IPokemonRepositoryJSON {
-    fun getPokemon(context: Context) : Pokemon
-}
